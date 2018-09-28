3fc25b50c092e2c330e879888bb6a41379567f1c
WilliamKluge <klugewilliam@gmail.com>
2018-09-28 12:36:16 -0400

    Updated dependencies for geotools

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
+        'Toin0u\Geotools\GeotoolsServiceProvider',
 
         /*
          * Application Service Providers...
@@ -208,6 +209,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Geotools' => 'Toin0u\Geotools\Facade\Geotools',
 
     ],
 
