130b086e72782cab9a6c9a6710135cc6d3d55cd8
WilliamKluge <klugewilliam@gmail.com>
2018-09-28 11:48:11 -0400

    Starting point - Lab 5

---
+++ app/Http/Controllers/DataRetrieval/StuffController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: william
+ * Date: 9/26/18
+ * Time: 2:46 PM
+ */
+
+namespace App\Http\Controllers\DataRetrieval;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\View;
+
+
+class StuffController extends Controller {
+
+    public static function get_stuff()
+    {
+//        $pres = DB::select('select * from presidents WHERE id=1');
+        $pres = DB::table('presidents')->where('id', 1)->first();
+
+        $president = array(
+            'fname'=>$pres->fname,
+            'lname'=>$pres->lname,
+            'dob'=>$pres->dob
+        );
+
+//        $president = 'test';
+
+//        return compact($president);
+//        return $president;
+        return View('welcome', compact('president'));
+//        return View::make("welcome")->with(array('presidents'=>$presidents));
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+|
+| TRY TO NOT MOVE ITEMS FROM THE LINES THEY ARE ON, phpDocumentor EXAMPLE LINKS TO THOSE LINES
+*/
+
+# Home page route
+Route::get('/', function () {
+    return \App\Http\Controllers\DataRetrieval\StuffController::get_stuff();
+});
+
+
+# Route to see information on presidents
+Route::get('/presidents', function () {
+    return \App\Http\Controllers\DataRetrieval\PresidentsController::get_presidents();
+});
