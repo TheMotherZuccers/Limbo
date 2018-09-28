bd437a018a496a296667ac52c9a1bacad5ac3b5f
WilliamKluge <klugewilliam@gmail.com>
2018-09-28 11:48:11 -0400

    Starting point - Lab 5

---
+++ resources/views/presidents.blade.php
@@ -0,0 +1,95 @@
+<!doctype html>
+
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Limbo</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+
+    <div class="content">
+        <div class="title m-b-md">
+            Dead Presidents
+        </div>
+
+        @section('presidents')
+            <table style="width:100%" border="1">
+                <tr>
+                    <th>President Number</th>
+                    <th>Firstname</th>
+                    <th>Lastname</th>
+                </tr>
+                @foreach($presidents as $pres)
+                    <tr>
+                        <td>{{ $pres->num }}</td>
+                        <td>{{ $pres->fname }}</td>
+                        <td>{{ $pres->lname }}</td>
+                    </tr>
+                @endforeach
+            </table>
+        @show
+
+    </div>
+</div>
+</body>
+</html>
