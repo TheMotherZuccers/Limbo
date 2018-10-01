88bad71050362d5ac833f3da872d35bf2c2f6237
WilliamKluge <klugewilliam@gmail.com>
2018-10-01 00:24:48 -0400

    Item add form - working with DB

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Item;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Grimzy\LaravelMysqlSpatial\Types\Point;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller {
+
+    public static function add_item() {
+        return View('item_form');
+    }
+
+    /**
+     * Create a new Item instance
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request) {
+
+        $item = new Item;
+
+        $item->description = $request->description;
+        $item->notable_damage = $request->notable_damage;
+        $item->environment_found = $request->environment_found;
+        $item->position_found = new Point($request->pos_x, $request->pos_y);
+        $item->position_radius =  $request->position_radius;
+//        TODO Set position comment correctly if instead of selecting a predefined location, the admin clicks a building
+        $item->position_comment = $request->position_comment;
+        $item->finder_id = User::all()->where('email', $request->finder_email)->first()->id;
+        $item->admin_id = Auth::id();
+
+        $item->save();
+
+        return redirect('/');
+    }
+
+}
--- database/migrations/2018_09_30_221102_create_items_table.php
+++ database/migrations/2018_09_30_221102_create_items_table.php
@@ -14,11 +14,15 @@ class CreateItemsTable extends Migration {
         Schema::create('items', function (Blueprint $table) {
             $table->increments('id');
             $table->string('description')->comment('describe what the item looks like/what it is');
-            $table->string('notable_damage')->comment('note any damage to the item when it was received by the lost and found office');
+            $table->string('notable_damage')->nullable()->comment('note any damage to the item when it was received by the lost and found office');
             $table->enum('environment_found', ['inside', 'sunny', 'rain', 'snow', 'humid'])->comment('state the environment the item was found in');
             $table->point('position_found')->comment('Coordinates the item was found at');
             $table->integer('position_radius')->nullable()->comment('Radius around position_found the item may have been in');
             $table->string('position_comment')->nullable()->comment('added details on where the item was found');
+            $table->unsignedInteger('finder_id');
+            $table->foreign('finder_id')->references('id')->on('users');
+            $table->unsignedInteger('admin_id');
+            $table->foreign('admin_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
