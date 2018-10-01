29e0a2e8b9d9c5dae529a186f4c7f67c5ca07109
WilliamKluge <klugewilliam@gmail.com>
2018-10-01 17:00:39 -0400

    Added item map to homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
             <h1>{{ config('app.name') }}</h1>
             <h3>Lost and Found Done Right</h3>
             <div class="row">
-                {{--<div class="col-md-6">--}}
+                <div class="col-md-6">
                     <table class="table table-striped">
                         <thead>
                         <tr>
@@ -25,8 +25,24 @@
                         @endforeach
                         </tbody>
                     </table>
-                {{--</div>--}}
-                {{--<div class="col-md-6">2</div>--}}
+                </div>
+                <div class="col-md-6">
+                    <div id="mapid" style="height: 360px; width: 500px;"></div>
+                    <script>
+                        var mymap = L.map('mapid').setView([41.72212, -73.93417], 15);
+
+                        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+                            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                            maxZoom: 18,
+                            id: 'mapbox.streets',
+                            accessToken: 'pk.eyJ1Ijoid2lsbGlhbWtsdWdlIiwiYSI6ImNqbW04eXB5dzBna2szcW83ajdlb2xpcmwifQ.RdkpVNHpUdMLV-2GJlTGTQ'
+                        }).addTo(mymap);
+                                @foreach($items as $item)
+                        var marker = L.marker([{{  $item->position_found->getLat() }}, {{ $item->position_found->getLng() }}]).addTo(mymap);
+                        marker.bindPopup("<b>{{ $item->description }}</b><br>Time Found: {{ $item->created_at }}<br><a href=\'/item/{{ $item->id }}\'>Item Information</a><br>");
+                        @endforeach
+                    </script>
+                </div>
             </div>
         </div>
     </div>
