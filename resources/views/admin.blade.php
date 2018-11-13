6e8f5e38e2ef62c95075939ccf27e543e092b184
WilliamKluge <klugewilliam@gmail.com>
2018-11-13 16:19:06 -0500

    Fixed admin blade styling

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,45 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="title m-b-md">
-            You're on an admin only page
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script type="text/javascript">
+        $(function() {
+            $('table.clickable-table').on("click", "tr.table-tr", function() {
+                window.location = $(this).data("url");
+            });
+        });
+    </script>
 
-            {{-- TODO admin can update status of items --}}
-            <div class="col-md-6">
-                    <table class="table table-striped" id="item_table">
-                        <thead>
-                        <tr>
-                            <th scope="col">Item ID</th>
-                            <th scope="col">Description</th>
-                            <th scope="col">Time Entered</th>
-                            <th scope="col">Notable Damage</th>
-                            <th scope="col">Environment Found</th>
-                            <th scope="col">Position X</th>
-                            <th scope="col">Position Y</th>
-                            <th scope="col">Position Comment</th>
-                            <th scope="col">Finder Email</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($items as $item)
-                            <tr>
-                                <td scope="row">{{ $item->id }}</td>
-                                <td>{{ $item->description }}</td>
-                                <td>{{ $item->created_at }}</td>
-                                <td>{{ $item->notable_damage }}</td>
-                                <td>{{ $item->environment_found }}</td>
-                                <td>{{ $item->pos_x }}</td>
-                                <td>{{ $item->pos_y }}</td>
-                                <td>{{ $item->position_comment }}</td>
-                                <td>{{ $item->finder_email }}</td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            
-            
-            {{-- TODO admin can remove items from view --}}
+    <style>
+        #item_table tr {
+            border-top: 1px solid #fff;
+        }
+        #item-table-body tr:hover {
+            background-color: #ccc;
+        }
+        #item-table-body th, #example td {
+            padding: 3px 5px;
+        }
+        #item-table-body td:hover {
+            cursor: pointer;
+        }
+    </style>
 
-        </div>
+    <div>
+        <h4>Click on any item edit it</h4>
+        <table class="table table-striped clickable-table" id="item_table">
+            <thead>
+            <tr>
+                <th scope="col">Description</th>
+                <th scope="col">Date Entered</th>
+                <th scope="col">Last Updated</th>
+                <th scope="col">Comment</th>
+                <th scope="col">Reporter's Email</th>
+            </tr>
+            </thead>
+            <tbody id="item-table-body">
+            @foreach($items as $item)
+                <tr class='table-tr' data-url="/item/{{ $item->id }}">
+                    <td>{{ $item->description }}</td>
+                    <td>{{ $item->created_at }}</td>
+                    <td>{{ $item->updated_at }} </td>
+                    <td>{{ $item->position_comment }}</td>
+                    <td>{{ $item->finder_email }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+
 @endsection
