6c4dbb3aa2758176ac324683fd31a4ec6aa22b06
WilliamKluge <klugewilliam@gmail.com>
2018-11-26 16:25:13 -0500

    Added item claim model

---
+++ app/ItemClaim.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model {
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+
+    public function claimed() {
+
+    }
+
+    public function returned() {
+
+    }
+
+}
